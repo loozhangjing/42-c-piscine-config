50377a18aadafa2c1abe1e204dd5c034ed68c624
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-04-17 18:34:58 +0800

    disable relative line numbers

--- options.lua
+++ options.lua
@@ -3,6 +3,7 @@
 -- Add any additional options here
 local opt = vim.opt
 
-opt.expandtab = false --- don't use spaces instead of tabs
-opt.tabstop = 4       --- width of a tab character
-opt.shiftwidth = 4    --- width of an indent
+opt.expandtab = false      --- don't use spaces instead of tabs
+opt.tabstop = 4            --- width of a tab character
+opt.shiftwidth = 4         --- width of an indent
+opt.relativenumber = false --- relative line numbers
